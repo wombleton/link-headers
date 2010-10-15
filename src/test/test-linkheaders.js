23b384985597e79f27b457db64ee90efef22a139
Rowan Crawford <wombleton@gmail.com>
2010-10-15 16:59:31 +1300

    Added test to make sure that a bad ref returns nothing

--- src/test/test-linkheaders.js
+++ src/test/test-linkheaders.js
@@ -5,6 +5,7 @@ test("header parsing", function() {
   ok(parse(header), 'We get something truthy back from parsing');
   ok(parse(header).find, 'We get a find function on the return of parsing');
   ok(parse(header).find('baz'), 'We get something truthy back from find')
+  ok(!parse(header).find('floopadoop'), 'We do not get something by passing a bad ref');
   ok(parse(header).find('foz').href(), 'We get something truthy back from calling href on a found link')
   equals(parse(header).find('foz').href().expand(), '/collection/', 'We get /collection/ from expanding href w/o args')
   equals(parse(header).find('baz').href().expand({ fozId: 'xxx'}), '/fozzes/xxx', 'We get /fozzes/xxx from expanding href w/ args')
